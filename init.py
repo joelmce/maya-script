b7fc2f7ae4da70827d99cecd545a7800861fa8fc
joelmce <joelanthony.mac@gmail.com>
2022-01-16 11:22:37 +1100

    Attributes and refactoring

--- init.py
+++ init.py
@@ -4,6 +4,11 @@ import random as random
 import json
 import numpy.random import choice
 
+# custom library
+import dna
+import rarity
+import attribute
+
 filePath = "/assets" # TODO: Filing heirarchy
 fileType = "obj"
 
@@ -48,60 +53,6 @@ class FileManager:
         else:
             for i in files:
                 cmds.file(filePath + i, i = True)
-
-class Rarity:
-    def __init(self, path, json):
-        with open(path + '/' + json) as json_file:
-            self.rarities = json.load(json_file)
-
-    def pick_attributes(self, category):
-        choices = list(self.rarities[category].keys())
-        probabilities = list(map(lambda x: x / 100.0, list(self.rarities[category].values())))
-        draw = choice(choices, 1, p = probabilities)[0]
-        return draw        
-
-class Generator:
-    DNA_DELIMITER = "-"
-
-    def __init__(self, supply):
-        self.supply = supply
-
-    def createDNA(_layers):
-        randNum = []
-        for layer in _layers:
-            totalWeight = 0
-
-            for element in layer.elements:
-                totalWeight += element.weight
-
-            random = math.floor(random.randint(1, 999) * totalWeight)
-
-            i = 0
-            while i < len(layer.elements):
-                random -= layer.elements[i].weight
-                if(random < 0):
-                    return randNum.push(layer.elements[i])
-        return randNum.join(DNA_DELIMITER)
-
-    def filterDNAOptions(_dna):
-        dnaItems = _dns.split(DNA_DELIMITER)
-        filteredDNA = filter(queryDNA, element) # Needs revision
-        return filteredDNA.join(DNA_DELIMITER)
-
-    def removeQueryStrings(_dna):
-        query = /(\?.*$/)
-        return _dna.replace(query, "")
-
-    def queryDNA():
-        query = /(\?.*$/)
-        queryString = query.exec(element)
-        if not queryString:
-            return True
-
-    def isDNAUnique(_dnaList = set(), _dna = ""):
-        _filteredDNA = filterDNAOptions(_dna)
-        if _filteredDNA not in _dnaList:
-            return True
 
 
 myWindow = Window()
